533f4df67dddbed5fe9bace8e1ccddfa054d28fb
Pierre <developmentPmnt@gmail.com>
2019-02-26 14:16:10 +0100

    2_(mvc)_ADD_Vegetables

---
+++ app/views/vegetables/_vegetable.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! vegetable, :id, :name, :user_id, :created_at, :updated_at
+json.url vegetable_url(vegetable, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_180706) do
+ActiveRecord::Schema.define(version: 2019_02_26_131105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,5 +39,14 @@ ActiveRecord::Schema.define(version: 2019_02_23_180706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vegetables", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vegetables_on_user_id"
+  end
+
   add_foreign_key "events", "users"
+  add_foreign_key "vegetables", "users"
 end
