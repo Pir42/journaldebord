94da63b8b0a35c1863182c078555fbc22ae37262
1pouniette <elise.dugue@hotmail.fr>
2019-03-11 17:01:45 +0100

    4_(c)_EDIT_french_locals_french_translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to '/403'
       end
+
+	  protect_from_forgery with: :exception
+	  before_action :set_locale
+
+	  private
+
+	  def set_locale
+	    I18n.locale = "fr"
+	  end
+
 end
