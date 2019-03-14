27dc7e6a06f8815179bb89c2fa07fe96c60989a7
Pierre <developmentPmnt@gmail.com>
2019-03-14 12:49:56 +0100

    7_(v)_EDIT_Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @follows = Event.where(user: current_user) + Fertilization.where(user: current_user) + Treatment.where(user: current_user)
+    add_breadcrumb "Dashboard"  
   end
 end
