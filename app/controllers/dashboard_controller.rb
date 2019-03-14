0da63edb6311cb08c7ae7798434cca6b46f396e7
Manautbertonboitel <manaut.bertonboitel@mmibordeaux.com>
2019-03-14 13:10:18 +0100

    7_(c)_FIX_Reidrect log in page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
   def index
     @follows = Event.where(user: current_user) + Fertilization.where(user: current_user) + Treatment.where(user: current_user)
     add_breadcrumb "Dashboard"  
